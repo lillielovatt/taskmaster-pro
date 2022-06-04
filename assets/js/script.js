2f24b18a6b6019ca89ce0d3527d8281d156ff219
lillielove <lillielove@gmail.com>
2022-06-04 11:30:29 -0700

    enable drag and drop features for tasks, update local storage accordingly

--- assets/js/script.js
+++ assets/js/script.js
@@ -183,4 +183,60 @@ $("#remove-tasks").on("click", function () {
 // load tasks for the first time
 loadTasks();
 
+$(".card .list-group").sortable({
+    connectWith: $(".card .list-group"),
+    scroll:false,
+    tolerance:"pointer",
+    helper:"clone", //create a copy of the dragged element and move the copy instead of the original
+    activate:function(event){
+        console.log("activate", this);
+    },
+    deactivate: function(event){
+        console.log("deactivate", this);
+    },
+    over: function(event){
+        console.log("over",event.target);
+    },
+    out: function(event){
+        console.log("out", event.target);
+    },
+    update: function(event){
+        var tempArr=[];
+        $(this).children().each(function(){
+            var text=$(this)
+                .find("p")
+                .text()
+                .trim();
+            var date=$(this)
+                .find("span")
+                .text()
+                .trim();
+            tempArr.push({
+                text:text,
+                date:date
+            })
+        })
+        var arrName=$(this)
+            .attr("id")
+            .replace("list-","");
+        // update array on tasks object and save
+        tasks[arrName]=tempArr;
+        saveTasks();
+        // console.log(tempArr);
+    }
+});
 
+$("#trash").droppable({
+    accept: ".card .list-group-item",
+    tolerance:"touch",
+    drop: function(event, ui){
+        console.log("drop");
+        ui.draggable.remove();
+    },
+    over: function(event, ui){
+        console.log("over");
+    },
+    out: function(event,ui){
+        console.log("out");
+    }
+})
